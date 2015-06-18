0c99fee6168f64cfff69435c3333463d4950326a
Trinh Minh Cuong <cuong@techmaster.vn>
2015-06-18 11:12:12 +0700

    Initial Commit

---
+++ demoGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demoGit
+//
+//  Created by cuong minh on 6/18/15.
+//  Copyright (c) 2015 Techmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demoGit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  demoGit
+//
+//  Created by cuong minh on 6/18/15.
+//  Copyright (c) 2015 Techmaster. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
